1ddb28825d7cab53f82f059866b2fb85b1401fd3
Florian Paul Schmidt <mista.tapas@gmx.net>
2013-06-24 11:53:10 +0200

    add the python thingie

---
+++ teq/python.cc
@@ -0,0 +1,38 @@
+#include <teq/teq.h>
+
+
+BOOST_PYTHON_MODULE(libteq)
+{
+	using namespace boost::python;
+	
+	class_<teq::midi_note_on_event>("note_on", init<unsigned, unsigned, unsigned>())
+	;
+	
+	class_<teq::midi_note_off_event>("note_off", init<unsigned, unsigned, unsigned>())
+	;
+	
+	class_<teq::midi_cc_event>("cc", init<unsigned, unsigned, unsigned>())
+	;
+	
+	class_<teq::midi_all_notes_off_event>("all_notes_off", init<unsigned>())
+	;
+	
+	class_<teq::track>("track")
+		.def("add_note_on", &teq::track::add_note_on)
+		.def("add_note_off", &teq::track::add_note_off)
+		.def("add_all_notes_off", &teq::track::add_note_off)
+		.def("add_cc", &teq::track::add_cc)
+		.def_readwrite("events", &teq::track::m_events)
+	;
+	
+	class_<teq::track::range>("track_range")
+		.def_readwrite("enabled", &teq::track::range::m_enabled)
+		.def_readwrite("start", &teq::track::range::m_start)
+		.def_readwrite("end", &teq::track::range::m_end)
+	;
+	
+	class_<teq::teq>("teq", init<std::string, unsigned>())
+		.def("set_loop_range", &teq::teq::set_loop_range)
+		.def("set_track", &teq::teq::set_track)
+    ;
+}
