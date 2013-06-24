30f5d3052120f5feb3dfd120a5f30b2e6c6d7711
Florian Paul Schmidt <mista.tapas@gmx.net>
2013-06-24 23:15:16 +0200

    some testing stuff

--- test_teq.py
+++ test_teq.py
@@ -8,9 +8,13 @@ t = libteq.teq("foo", 100)
 t.set_loop_range(r)
 
 tr = libteq.track()
+
 tr.add_note_on(0, 0, 64, 64)
 tr.clear_range(0, 48000)
-tr.add_note_on(0, 0, 64, 64)
+
+for n in range(0, 512):
+	tr.add_note_on(n * 48000/512, 0, 64, 64)
+	
 t.set_track(tr)
 
 try:
