870dc2bfd66b466157ea4e267071febb664f0c04
Florian Paul Schmidt <mista.tapas@gmx.net>
2014-01-26 01:41:03 +0100

    partial jack_transport support

--- teq/teq.cc
+++ teq/teq.cc
@@ -911,7 +911,7 @@ namespace teq
 			 * Here come all the state transitions that depend on the ticks
 			 * and not individual frames.
 			 */
-			if (m_transport_state == transport_state::PLAYING && m_time_until_next_tick <= 0.0)
+			if (m_transport_state == transport_state::PLAYING && m_time_until_next_tick <= 0.001)
 			{
 				process_tick(m_transport_position, frame_index, multi_out_buffer, patterns);
 
